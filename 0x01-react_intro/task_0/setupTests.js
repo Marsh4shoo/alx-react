3ad09095f33fd5609deeed41dc664b0c65eb2ea0
Marsh4shoo <marshal.wjnr@gmail.com>
2024-08-12 21:01:57 +0000

    Updated

--- 0x01-react_intro/task_0/setupTests.js
+++ 0x01-react_intro/task_0/setupTests.js
@@ -1,5 +1,6 @@
+// src/setupTests.js
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16'; // or the appropriate adapter version
 
 configure({ adapter: new Adapter() });
 
